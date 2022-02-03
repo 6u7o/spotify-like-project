18d4dc601b1dbb40036de16430312effbd670c86
Guto <augustopinheiro19999999@gmail.com>
2022-02-03 19:48:57 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,18 +7,82 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
+import Loading from './components/Loading';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      info: { name: null },
+      isDisabled: true,
+      logged: false,
+      received: false,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+    const minInput = 3;
+    if (value.length >= minInput) {
+      this.setState({
+        isDisabled: false,
+      });
+    }
+    this.setState({
+      info: { name: value },
+    });
+  }
+
+  onClick = async () => {
+    this.setState({
+      logged: true,
+    });
+    const { info } = this.state;
+    const response = await createUser(info);
+    if (response === 'OK') {
+      return this.setState({
+        logged: false,
+        received: true,
+      });
+    }
+  }
+
   render() {
+    const {
+      logged,
+      info,
+      isDisabled,
+      received,
+    } = this.state;
+
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Login } />
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="*" component={ NotFound } />
+        <Switch>
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          {logged ? <Loading /> : null}
+          {received ? <Redirect to="/search" /> : null}
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              <Login
+                infoP={ info }
+                isDisabledP={ isDisabled }
+                handleClick={ this.onClick }
+                handleChange={ this.onInputChange }
+                loggedP={ logged }
+                receivedP={ received }
+              />
+            ) }
+          />
+
+          <Route exact path="*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
   render() {
+    const {
+      isDisabledP,
+      handleClick,
+      handleChange,
+    } = this.props;
+
     return (
       <div
         data-testid="page-login"
       >
-        login
+        <label htmlFor="nameInput">
+          <input
+            id="nameInput"
+            data-testid="login-name-input"
+            onChange={ handleChange }
+          />
+        </label>
+        <button
+          id="btnForm"
+          type="button"
+          aria-label="button"
+          data-testid="login-submit-button"
+          disabled={ isDisabledP }
+          onClick={ handleClick }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  isDisabledP: PropTypes.bool.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 
 export default Login;
