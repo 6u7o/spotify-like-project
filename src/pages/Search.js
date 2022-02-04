e4021bb687db7dd505eb4a6916efd8ce07e571d0
Guto <augustopinheiro19999999@gmail.com>
2022-02-04 16:38:07 -0300

    Requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       btnDisabled: true,
+      inputValue: '',
+      loading: false,
+      responseText: '',
+      arrAlb: [],
     };
   }
 
@@ -18,14 +25,93 @@ class Search extends React.Component {
         btnDisabled: false,
       });
     }
+    this.setState({
+      inputValue: value,
+    });
+  }
+
+  clickSearchRequest = async () => {
+    const { inputValue } = this.state;
+    this.setState({
+      loading: true,
+    });
+    const response = await searchAlbumsAPI(inputValue);
+    if (response) {
+      this.setState({
+        loading: false,
+        responseText: `Resultado de álbuns de: ${inputValue}`,
+        arrAlb: response,
+      });
+    }
+    // console.log(response);
+    this.setState({
+      inputValue: '',
+    });
   }
 
   render() {
     const { userNamePage } = this.props;
-    const { btnDisabled } = this.state;
+
+    const {
+      btnDisabled,
+      inputValue,
+      loading,
+      responseText,
+      arrAlb,
+    } = this.state;
+
     return (
       <>
-        <Header userName={ userNamePage } />
+        {loading ? <Loading /> : (
+          <>
+            <Header userName={ userNamePage } />
+            <div
+              data-testid="page-search"
+            >
+              search
+            </div>
+            <form>
+              <input
+                value={ inputValue }
+                onChange={ this.handleChange }
+                data-testid="search-artist-input"
+              />
+              <button
+                disabled={ btnDisabled }
+                onClick={ this.clickSearchRequest }
+                type="button"
+                data-testid="search-artist-button"
+              >
+                Pesquisar
+              </button>
+            </form>
+            {arrAlb.length !== 0 ? responseText : null}
+            {arrAlb.length === 0 ? 'Nenhum álbum foi encontrado' : arrAlb.map((it) => (
+              <div
+                key={ it.collectionId }
+              >
+                {/* <h3>
+                  {it.collectionName}
+                </h3> */}
+                <Link
+                  data-testid={ `link-to-album-${it.collectionId}` }
+                  to={ `/album/${it.collectionId}` }
+                >
+                  {it.collectionName}
+                </Link>
+                <img
+                  src={ it.artworkUrl100 }
+                  alt={ it.collectionName }
+                />
+                <p>
+                  {it.artistName}
+                </p>
+              </div>
+            ))}
+          </>
+        )}
+
+        {/* <Header userName={ userNamePage } />
         <div
           data-testid="page-search"
         >
@@ -33,17 +119,19 @@ class Search extends React.Component {
         </div>
         <form>
           <input
+            value={ inputValue }
             onChange={ this.handleChange }
             data-testid="search-artist-input"
           />
           <button
             disabled={ btnDisabled }
+            onClick={ this.clickSearchRequest }
             type="button"
             data-testid="search-artist-button"
           >
             Pesquisar
           </button>
-        </form>
+        </form> */}
       </>
     );
   }
