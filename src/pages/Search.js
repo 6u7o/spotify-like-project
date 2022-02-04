233367184c8a4a414c55ad39014f631e814d0fbc
Guto <augustopinheiro19999999@gmail.com>
2022-02-04 15:00:03 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,8 +3,26 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      btnDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const minInput = 2;
+    if (value.length >= minInput) {
+      this.setState({
+        btnDisabled: false,
+      });
+    }
+  }
+
   render() {
     const { userNamePage } = this.props;
+    const { btnDisabled } = this.state;
     return (
       <>
         <Header userName={ userNamePage } />
@@ -13,6 +31,19 @@ class Search extends React.Component {
         >
           search
         </div>
+        <form>
+          <input
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            disabled={ btnDisabled }
+            type="button"
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </>
     );
   }
